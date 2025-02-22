1abf4ce5fb65455dfc2407f79e3bfdad87c74b5b
عاصم محمود <asim16111980@gmail.com>
2025-02-22 20:43:33 +0200

    Create Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Button from "./combonents/Button";
 import Input from "./combonents/Input";
 
 function App() {
@@ -16,26 +17,6 @@ function App() {
           success: { parent: "border-green-500 bg-green-50 text-green-700 ring-2 ring-green-300", input: "", label: "text-green-500" },
           error: { parent: "border-red-500 bg-red-50 text-red-700 ring-2 ring-red-300", input: "", label: "text-red-500" },
         }}
-        // stateStyles={{
-        //   enabled: "border-gray-300 bg-white text-gray-700",
-        //   hovered: "border-pink-400",
-        //   focused:
-        //     "border-rose-500 ring-2 ring-rose-300 shadow-lg shadow-rose-200",
-        //   populated:
-        //     "border-green-500 bg-green-50 text-green-700 ring-2 ring-green-300",
-        //   disabled:
-        //     "border-gray-200 bg-gray-100 text-gray-400 cursor-not-allowed",
-        //   error: "border-red-500 bg-red-50 text-red-700 ring-2 ring-red-300",
-        // }}
-        // labelStyles={{  enabled: " text-gray-700",
-        // hovered: "text-pink-400",
-        // focused:
-        //   "text-rose-500",
-        // populated:
-        //   "text-green-500",
-        // disabled:
-        //   "text-gray-200 cursor-not-allowed",
-        // error: "text-red-500",}}
       />
     </>
   );
---
+++ src/combonents/Button.tsx
@@ -0,0 +1,8 @@
+
+const Button = ({icon,value}) => {
+  return (
+      <button><span>{ icon}</span><span>{value }</span></button>
+  )
+}
+
+export default Button
